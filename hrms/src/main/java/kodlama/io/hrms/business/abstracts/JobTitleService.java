84209a3aa40912b54c47cce2809772183461bfd0
gunnurraksoy@gmail.com <gunnurraksoy@gmail.com>
2021-05-28 22:54:54 +0300

    7.day

---
+++ hrms/src/main/java/kodlama/io/hrms/business/abstracts/JobTitleService.java
@@ -0,0 +1,10 @@
+package kodlama.io.hrms.business.abstracts;
+
+import java.util.List;
+
+import kodlama.io.hrms.core.utilities.results.DataResult;
+import kodlama.io.hrms.entities.concretes.JobTitle;
+
+public interface JobTitleService {
+	DataResult<List<JobTitle>>getAll();
+}
---
+++ hrms/src/main/java/kodlama/io/hrms/business/concretes/JobTitleManager.java
@@ -0,0 +1,31 @@
+package kodlama.io.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlama.io.hrms.business.abstracts.JobTitleService;
+import kodlama.io.hrms.core.utilities.results.DataResult;
+import kodlama.io.hrms.core.utilities.results.SuccessDataResult;
+import kodlama.io.hrms.dataAccess.abstracts.JobTitleDao;
+import kodlama.io.hrms.entities.concretes.JobTitle;
+
+@Service
+public class JobTitleManager implements JobTitleService {
+	private JobTitleDao jobTitleDao;
+ 
+	@Autowired
+	public JobTitleManager(JobTitleDao jobTitleDao) {
+		super();
+		this.jobTitleDao = jobTitleDao;
+	}
+
+	@Override
+	public DataResult<List<JobTitle>> getAll() {
+		return new SuccessDataResult<List<JobTitle>>(this.jobTitleDao.findAll(),"Data listelendi");
+	}
+	
+	
+
+}
